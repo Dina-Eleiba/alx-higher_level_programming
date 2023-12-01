89dabbf36e1b8a1ff51572c427f0432a5071e66a
Dina-Eleiba <dina.adel@outlook.com>
2023-11-30 17:16:06 -0800

    oo

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from variable_load_5 import a
-    print("{}".format(a))
+    import hidden_4
+    files = dir(hidden_4)
+    for file in files:
+        if file[:2] != '__' and file[-1:-3] != '__':
+            print("{}".format(file))
